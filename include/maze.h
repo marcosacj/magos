03b85621fd47472418996e1b1daa03eb36c18e1d
Marcos Junior <marcos.acj@outlook.com>
2018-11-24 15:43:41 -0300

    Improve comments...

--- include/maze.h
+++ include/maze.h
@@ -75,7 +75,7 @@ public:
 	~Maze();
 
 	/// Knocks down a Wall.
-	/** Receives the coordinates of a cell and a target Wall (that is, the corresponding state)
+	/** Receives the coordinates of a cell and a target Wall (that is, the corresponding Wall enumerator)
 		to be knocked down. Note that corresponding walls of neighboor cells are also knocked down.
 		@param column Column of the target cell.
 		@param line Line of the targes cell.
@@ -86,7 +86,7 @@ public:
 	void knock_down( const Coord & column, const Coord & line, const Wall & targetWall );
 
 	/// Sets the state of a target cell.
-	/** ...
+	/** Receives the coordinates of a cell and a target State (that is, the corresponding State enumerator).
 		@param column Column of the target cell.
 		@param line Line of the targes cell.
 		@param targetState Target State to be set.
