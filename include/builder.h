04e933606c2db8aed929440d6fa1d3497e2f4b42
Marcos Junior <marcos.acj@outlook.com>
2018-12-10 02:28:48 -0300

    Doxygen comments on Builder

--- include/builder.h
+++ include/builder.h
@@ -11,6 +11,7 @@
 #include "hash.h"
 #include "roulette.h"
 
+/// This class implements a building method of a maze base on Hash Table.
 class HashBuilder {
 
 private:
@@ -18,27 +19,57 @@ private:
 	/// Pointer to maze to be built.
 	Maze * m;
 
-	/// Pointer to a render object.
+	/// Pointer to a render object used to make a png file of the maze.
 	Render * r;
 
-	/// Roulette to store shuffle cell indexes
+	/// Vector of index of the maze, which works like a linked list.
+	/** This object uses built-in methods to randomly traverse all cells of the maze
+		during building process.\n
+		It is initialized inside the constructor acording to dimensions of the maze. */
 	Roulette<Nat> c;
 
+	/// Pointer to a Hash Table object, which controls the building process.
+	/** This object uses built-in methods to control the building process through the idea of
+		groups of paths (represented by the keys of each value).\n
+		In this building strategy, two cells should have one, and only one, path connecting each other.\n
+		When this connection happens, the two cells are merged in the HashTable, having the same key.\n
+		The building process ends when all cells have one, and only one, path connecting them, two by two.\n
+		This situation happens when the Hash Table has only one element, with only one key. */
 	HashTable< Nat, Nat > * h;
 
-	// Convert from coordinates to index of a cell
+	/// Converts the location of a cell from coordinates to index.
+	/** Note this method does not do bounds checking, neither throws exceptions if coordinate are
+		outside the maze.\n
+		If necessary, the method `valid_coord()` may be used before calling this method.
+		@param column Column of the targer cell.
+		@param line Line of the target cell.
+		@return Index of the targed cell. */
 	inline Nat to_index( const Coord & column, const Coord & line ){ return m->get_wid() * line + column; };
 
-	/// Convert from index to column of a cell.
+	/// Retrieves the column coordinate of a cell from its index.
 	inline Coord to_column( const Nat & index ) { return index % m->get_wid(); };
 
-	/// Convert from index to line of a cell.
+	/// Retrieves the line coordinate of a cell from its index.
 	inline Coord to_line( const Nat & index ) { return floor( index / m->get_wid() ); };
 
-	// receives a cell (coorinates) and a wall and returns the index of the neighbor cell.
+	/// Calculates the index of the neighbor cell of a given cell and given wall.
+	/** This method receives the coordinates of the choosen cell.\n
+		Also, do bounds checking and throws	exceptions in invalid cases.
+		@param column Column of the given cell.
+		@param line Line of the given cell.
+		@param targetWall Target wall whose neighbor should be taken.
+		@throw std::runtime_error When the coordinates are not valid or outside the maze.
+		@throw std::runtime_error When the target wall is not valid.
+		@throw std::runtime_error When the target wall is a border of the maze (this case is composed
+			of 4 subcases). */
 	Nat neighbor( const Coord & column, const Coord & line, const Nat & targetWall );
 
-	// receives a cell (index) and a wall and returns the index of the neighbor cell.
+	/// Receives a cell (index) and a wall and returns the index of the neighbor cell.
+	/** This method is just an overload, for convenience of giving index of the choosen cell instead
+		its coordinates.\n
+		This just converts index to coordinates and calls the method which receives	coordinates as parameters.
+		@param index Index of the choosen cell.
+		@param targetWall Target wall whose neighbor should be taken. */
 	Nat neighbor( const Nat & index, const Nat & targetWall );
 
 	/// Checks if a coordinates pair points to inside the matrix.
@@ -50,17 +81,35 @@ private:
 public:
 
 	/// Default constructor.
+	/** This constructor updates maze and render pointers, allocates memory to HashTable object
+		and fills it with the values of the indexes of the maze.\n
+		Also, fills and shuffles the vector of indexes inside the roulette object.
+		@param m Pointer to the maze object.
+		@param r Pointer to the render object. */
 	HashBuilder( Maze * m, Render * r );
 
 	/// Default destructor.
 	~HashBuilder();
 
+	/// Returns the state of the maze acording to the building process.
+	/** This method may be use by an external controller of the building process.
+		@return True if the maz is built (that is, if the size of HashTable is just 1)
+		or False otherwise. */
 	inline bool is_built( void ) const { return h->size() == 1; };
 
-	/// Receives a pointer to a Maze object and turns it into a perfect Maze.
+	/// Building method for test purposes.
+	/** This method implements an example in how to control the building process. */
 	void build_maze( void );
 
-	// Comment
+	/// Advance one step in the building process, usually knock down a random wall.
+	/** This method performs one step in the building proceess.\n
+		It uses the shuffled vector of indexes to pick a random cell.\n
+		Then it choose a random wall and calculate the respective neighbor.\n
+		A connection checking is made through the HashTable object.\n
+		If there is no connection, the choosen wall is knock down, connecting the cells.\n
+		The boolean return value may be used to control when a snapshot of the maze should be
+		drawn to a png file, by an external controller of the process.
+		@return True if a wall was knocked down and False otherwise. */
 	bool build_step( void );
 
 };
